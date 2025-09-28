c1b44acce5027bcbd61fd1d07669a14e25811860
= <kanurag17780@gmail.com>
2025-09-28 19:15:42 +0530

    added header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import ListEmployeeComponents from './coponents/ListEmployeeComponents'
+import HeaderComponent from './coponents/HeaderComponent'
+import FooterComponent from './coponents/FooterComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <HeaderComponent/>
       <ListEmployeeComponents/>
+      <FooterComponent/>
+      
     </>
   )
 }
